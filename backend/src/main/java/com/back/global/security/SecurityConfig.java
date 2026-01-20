37c2cf9d88af5e4c3f305e4b4b0247d89447b72d
송찬의 <dforce103@gmail.com>
2026-01-20 09:48:09 +0900

    feat: 필터를 시큐리티와 연결

--- backend/src/main/java/com/back/global/security/SecurityConfig.java
+++ backend/src/main/java/com/back/global/security/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter CustomAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -40,6 +43,7 @@ public class SecurityConfig {
                         // CSRF 비활성화
                         AbstractHttpConfigurer::disable
                 )
+                .addFilterBefore(CustomAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         ;
 
         return http.build();
