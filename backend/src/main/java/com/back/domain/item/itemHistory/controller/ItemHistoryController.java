41052f8a92a731bbfd1eeca0eac19a7acfff9afd
Minji Kim <sml07113@gmail.com>
2026-01-22 16:06:31 +0900

    fix: 주석 및 불필요한 코드 제거

--- backend/src/main/java/com/back/domain/item/itemHistory/controller/ItemHistoryController.java
+++ backend/src/main/java/com/back/domain/item/itemHistory/controller/ItemHistoryController.java
@@ -7,14 +7,12 @@ import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -28,12 +26,7 @@ public class ItemHistoryController {
 
     @GetMapping("/histories")
     @Operation(summary = "전체 아이템 이력 조회", description = "전체 아이템의 이력을 일괄 조회합니다.")
-    public RsData<List<ItemAllHistoryResponse>> getAllItemHistories(HttpServletRequest request) {
-        System.out.println(Arrays.toString(request.getCookies()));
-//        [jakarta.servlet.http.Cookie@1da37b87{apiKey=bcb68f18-845b-4f2e-90f0-2bef3b9ada1d,null}]
-//        [jakarta.servlet.http.Cookie@f93a7567, jakarta.servlet.http.Cookie@e8053076, jakarta.servlet.http
-//        .Cookie@6356e01f, jakarta.servlet.http.Cookie@2c2707d6]
-
+    public RsData<List<ItemAllHistoryResponse>> getAllItemHistories() {
         Long userId = rq.getMemberId();
         List<ItemAllHistoryResponse> list = itemHistoryService.getAllItemHistories(userId);
 
