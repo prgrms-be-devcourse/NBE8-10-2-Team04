becac08cc8681232ee35174231ff1e21f30d29f9
BaekRaven <yjw70809@gmail.com>
2026-01-27 11:46:33 +0900

    feat:회원가입 이메일 검증 추가, 컨트롤러에 있던 로직 request,response에서 Dto로 이동

--- backend/src/main/java/com/back/domain/user/user/dto/UserJoinRequest.java
+++ backend/src/main/java/com/back/domain/user/user/dto/UserJoinRequest.java
@@ -5,18 +5,21 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-/**
- * 프로필(이메일) 수정 요청
- * PATCH /api/v1/user/me
- */
-public record UserProfileUpdateRequest(
+//회원가입
+public record UserJoinRequest(
         @NotBlank
         @Size(min = 2, max = 30)
-        @Email
+        String loginId,
+        @NotBlank
+        @Size(min = 2, max = 30)
+        String password,
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
         @Pattern(
                 regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
                 message = "올바른 이메일 형식을 입력해주세요."
         )
+        @NotBlank
+        @Size(min = 2, max = 30)
         String email
 ) {
 }
